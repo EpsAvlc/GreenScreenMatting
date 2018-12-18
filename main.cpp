c2860a4eb86c905e16e0eb188e77f265389829d1
EpsAvlc <cm9999@qq.com>
2018-12-18 22:58:51 +0800

    init project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
+using namespace std;
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl;
     return 0;
 }
